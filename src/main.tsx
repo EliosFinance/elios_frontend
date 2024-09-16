484a2262dcda74f1d27951d49e63bda7f707bd49
Florian PALVADEAU <florian.palvadeau@edu.devinci.fr>
2024-09-16 20:23:16 +0200

    Feat(Main.tsx): FIX minor re-render glitch

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './css/index.css'
-import {BrowserRouter} from "react-router-dom";
-import {QueryClient, QueryClientProvider} from "react-query";
-import AuthProvider from "@/context/AuthProvider.tsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './css/index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import AuthProvider from '@/context/AuthProvider.tsx';
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 export const APP_ROUTES_ENUM = {
   HOME: '/',
@@ -15,17 +15,23 @@ export const APP_ROUTES_ENUM = {
   ARTICLE_CATEGORIES: '/article-categories',
   ARTICLE_CATEGORY: '/article-category',
   LOGIN: '/login',
+};
+
+const container = document.getElementById('root');
+if (!container) {
+  throw new Error('Root container missing in index.html');
 }
 
+const root = ReactDOM.createRoot(container);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+root.render(
   <React.StrictMode>
-      <BrowserRouter>
-          <AuthProvider>
-            <QueryClientProvider client={queryClient}>
-                <App />
-            </QueryClientProvider>
-          </AuthProvider>
-      </BrowserRouter>
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <AuthProvider>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
