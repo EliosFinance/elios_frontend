9f1ca024c47ce010afee1a3d51257b8a966b6e77
seinokoula <alexandre.charles92@gmail.com>
2024-11-08 16:21:19 +0100

    :round_pushpin: Change routing for the page

--- src/main.tsx
+++ src/main.tsx
@@ -15,7 +15,7 @@ export const APP_ROUTES_ENUM = {
     ARTICLE_CATEGORIES: '/article-categories',
     ARTICLE_CATEGORY: '/article-category',
     LOGIN: '/login',
-    DEFI: '/defi',
+    DEFI: '/challenge',
 };
 
 const container = document.getElementById('root');
