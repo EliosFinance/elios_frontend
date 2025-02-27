8d55d96f9c6cceea67398ba42634a98747a85d66
mely-h <melissa.hanouti@edu.devinci.fr>
2025-02-27 20:49:34 +0100

    feat(pages): Politique  de confidentialite

--- src/pages/Settings/Confidentialite/PolitiqueConfidentialite.tsx
+++ src/pages/Settings/Confidentialite/PolitiqueConfidentialite.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { ArrowLeft, List, ArrowDown } from "lucide-react";
 import ReactMarkdown from "react-markdown";
