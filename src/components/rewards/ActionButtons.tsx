14708e3b10e08dfd6447f1e2d6ac3ac8449e6caa
Florian PALVADEAU <florian.palvadeau@edu.devinci.fr>
2025-04-28 21:55:50 +0200

    feat(components): WIP

--- src/components/rewards/ActionButtons.tsx
+++ src/components/rewards/ActionButtons.tsx
@@ -1,14 +1,16 @@
 // components/ActionButtons.tsx
-import { Button } from '@/components/ui/Button';
 import { Drawer, DrawerClose, DrawerContent, DrawerHeader, DrawerTitle, DrawerTrigger } from '@/components/ui/drawer';
+import { Button } from '../ui/button';
 
 const ActionButtons = () => {
     return (
-        <div className='flex justify-between mb-4'>
-            <Button className='flex-1 px-4 py-2 mr-2 text-white bg-blue-500 rounded'>Activité</Button>
+        <div className='flex justify-between mb-4 gap-2'>
+            <Button className='flex-1 px-4 py-2 mr-2 h-12 text-white bg-blue-500 rounded-lg text-lg'>Activité</Button>
             <Drawer>
                 <DrawerTrigger asChild>
-                    <Button className='flex-1 px-4 py-2 text-white bg-blue-500 rounded'>Comment ça marche</Button>
+                    <Button className='flex-1 px-4 py-2 h-12 text-white bg-blue-500 rounded-lg text-lg'>
+                        Comment ça marche
+                    </Button>
                 </DrawerTrigger>
                 <DrawerContent>
                     <DrawerHeader>
--- src/components/rewards/EliCoinsCard.tsx
+++ src/components/rewards/EliCoinsCard.tsx
@@ -1,3 +1,5 @@
+import { MedalIcon } from 'lucide-react';
+
 // components/EliCoinsCard.tsx
 interface EliCoinsCardProps {
     coins: number;
@@ -8,19 +10,16 @@ const EliCoinsCard = ({ coins, totalCoins }: EliCoinsCardProps) => {
     const percentage = (coins / totalCoins) * 100;
 
     return (
-        <div className='p-4 mb-4 bg-gray-100 rounded'>
-            <div className='flex items-center justify-between mb-2'>
-                <div className='flex items-center'>
-                    <img
-                        src='https://images.unsplash.com/photo-1574607407517-cd664b1504f5?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y29pbnN8ZW58MHx8MHx8fDA%3D'
-                        alt='EliCoins'
-                        className='w-16 h-16 mr-2'
-                    />
-                    <span className='font-bold'>Mes EliCoins</span>
+        <div className='h-20 p-4 mb-4 bg-[--neutral-800] border border-solid border-[--neutral-500] rounded-lg flex flex-col justify-between'>
+            <div className='flex items-center justify-between mb-2 gap-2'>
+                <MedalIcon className='w-10 h-10 text-blue-500' />
+                <div className='w-full h-full flex items-start justify-center flex-col'>
+                    <span className='font-bold text-sm'>Mes EliCoins</span>
+                    <h6 className='text-4xl'>
+                        <b>{coins}</b>
+                        <span className='text-[--neutral-500]'>/{totalCoins}</span>
+                    </h6>
                 </div>
-                <span className='font-bold'>
-                    {coins}/{totalCoins}
-                </span>
             </div>
             <div className='h-2 bg-blue-200 rounded-full'>
                 <div className='h-2 bg-blue-500 rounded-full' style={{ width: `${percentage}%` }}></div>
